8ebbed24a17ada0fd2156cdf1b886d43bbaa985e
Jonas hjemme PC <jhdank@gmail.com>
2023-02-24 15:36:38 +0100

    Dette er markedsplassen, er i App.js foreløpig

--- reactclient/src/App.js
+++ reactclient/src/App.js
@@ -1,11 +1,82 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [posts, setPosts] = useState({});
+
+  function getPosts() {
+    const url = "https://localhost:7107/get-all-posts";
+
+    fetch(url, {
+      method: "GET",
+    })
+      .then((response) => response.json())
+      .then((postsFromServer) => {
+        console.log(postsFromServer);
+        setPosts(postsFromServer);
+      })
+      .catch((error) => {
+        console.log(error);
+        alert(error);
+      });
+  }
+
   return (
-    <div>
-      Hello world
+    <div className="container">
+      <div className="row min-vh-100">
+        <div className="col d-flex flex-column justify-content-center align-items-center">
+          <div>
+            <h1>APP2000 Prosjekt</h1>
+
+            <div className="mt-5">
+              <button onClick={getPosts} className="btn btn-dark btn-lg w-100">
+                Få innleggene fra server
+              </button>
+              <button
+                onClick={() => {}}
+                className="btn btn-secondary btn-lg w-100 mt-4"
+              >
+                Lag nytt innlegg
+              </button>
+            </div>
+          </div>
+
+          {posts.length > 0 && renderPostsTable()}
+        </div>
+      </div>
     </div>
   );
+
+  function renderPostsTable() {
+    return (
+      <div className="table-responsive mt-5">
+        <table className="table table-bordered border-dark">
+          <thead>
+            <tr>
+              <th scope="col">PostId (PK)</th>
+              <th scope="col">Title</th>
+              <th scope="col">Content</th>
+              <th scope="col">CRUD OPerations</th>
+            </tr>
+          </thead>
+          <tbody>
+            {posts.map((post) => (
+              <tr key={post.postId}>
+                <th scope="row">{post.postId}</th>
+                <td>{post.title}</td>
+                <td>{post.content}</td>
+                <td>
+                  <button className="btn btn-dark btn-lg mx-3 my-3">Update</button>
+                  <button className="btn btn-secondary btn-lg">Delete</button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+
+        <button onClick={() => setPosts([])} className="btn btn-dark btn-lg w-100">Tøm innlegg array</button>
+      </div>
+    );
+  }
 }
 
 export default App;
