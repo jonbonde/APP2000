5b20a97975c63a48a3aeaab5816c4642bbf8edb5
Andreasj3 <andreasjsendal@gmail.com>
2023-03-06 20:57:14 +0100

    Fjernet gamle knapper, og la til nye knapper

--- reactclient/src/Pages/WireframeTool.js
+++ reactclient/src/Pages/WireframeTool.js
@@ -1,84 +1,239 @@
 import "../Utilities/Stylesheet.css";
-import HamburgerMenu from './HamburgerMenu';
+import HamburgerMenu from "./HamburgerMenu";
 
 function WireframeTool() {
   return (
     <div className="background">
-      <div className="customWebpage"></div>
-      <div className="dropdown1">
-        <label htmlFor="menu1" className="white-text">
-          Titles
-        </label>
-        <select id="menu1">
-          <option value="option1">Option 1</option>
-          <option value="option2">Option 2</option>
-          <option value="option3">Option 3</option>
-        </select>
-      </div>
-      <div className="dropdown2">
-        <label htmlFor="menu2" className="white-text dropdown-label">
-          Fonts
-        </label>
-        <select id="menu2">
-          <option value="option1">Option 1</option>
-          <option value="option2">Option 2</option>
-          <option value="option3">Option 3</option>
-        </select>
-      </div>
-      <div className="dropdown3">
-        <label htmlFor="menu3" className="white-text">
-          Pictures
-        </label>
-        <select id="menu3">
-          <option value="option1">Option 1</option>
-          <option value="option2">Option 2</option>
-          <option value="option3">Option 3</option>
-        </select>
-      </div>
-      <div className="dropdown4">
-        <label htmlFor="menu4" className="white-text">
-          Navbar
-        </label>
-        <select id="menu4">
-          <option value="option1">Option 1</option>
-          <option value="option2">Option 2</option>
-          <option value="option3">Option 3</option>
-        </select>
-      </div>
-      <div className="dropdown5">
-        <label htmlFor="menu5" className="white-text ">
-          Footer
-        </label>
-        <select id="menu5">
-          <option value="option1">Option 1</option>
-          <option value="option2">Option 2</option>
-          <option value="option3">Option 3</option>
-        </select>
+      
+      <div className="customWebpage">
+
       </div>
 
-      <h1 className="heading">Design your own webpage!</h1>
+      <h1 className="heading">Design your own webpage!
+      </h1>
       <p className="smallerHeading">
         Get started by choosing from the several options
       </p>
 
-    <div className="">
-      <p>Hallo</p>
-    </div>  
+      <nav class="navbarRight"> 
+        <ul>
+          <li>
+            <button onClick={handleTitlesButtonClick}>Titles</button>
+          </li>
+          <li>
+            <button onClick={handleFontsButtonClick}>Fonts</button>
+          </li>
+          <li>
+            <button onClick={handlePicturesButtonClick}>Pictures</button>
+          </li>
+          <li>
+            <button onClick={handleNavbarButtonClick}>Navbar</button>
+          </li>
+          <li>
+            <button>Footer</button>
+          </li>
+          <li>
+            <button>Text</button>
+          </li>
+          <button onClick={handleResetButtonClick}>Reset</button>
+        </ul>
+      </nav>
 
-
-    <HamburgerMenu>
-                    
-    </HamburgerMenu> 
-
+      <HamburgerMenu></HamburgerMenu>
     </div>
-
-
-    
-    
-
   );
 }
 
+// Reset button, fjerner titler, bilder og navbar.
 
+function handleResetButtonClick() {
+  const textElement = document.querySelector("pTitles");
+  if (textElement) {
+    textElement.remove();
+  }
+  const pictureElement = document.querySelector("divPicture");
+  if (pictureElement) {
+    pictureElement.remove();
+  }
+  const navbarElement = document.querySelector("divNavbar");
+  if (navbarElement) {
+    navbarElement.remove();
+  }
+}
+
+// Titles button, legger til tekst på skjermen.
+
+function handleTitlesButtonClick() {
+  const titleText = "Hello, world!";
+  const textElement = document.createElement("pTitles");
+  textElement.innerText = titleText;
+  textElement.style.position = "absolute";
+  textElement.style.top = "32%";
+  textElement.style.left = "42%";
+  textElement.style.transform = "translate(-50%, -50%)";
+  document.body.appendChild(textElement);
+}
+
+// Fonts knapp, endrer fonten til titlen.
+
+function handleFontsButtonClick() {
+  const textElement = document.querySelector("pTitles");
+  if (textElement) {
+    const fonts = [
+      "Arial, sans-serif",
+      "Georgia, serif",
+      "Impact, Charcoal, sans-serif",
+      "Times New Roman, Times, serif",
+      "Courier New, Courier, monospace",
+    ];
+
+    const currentFontIndex = fonts.indexOf(textElement.style.fontFamily);
+    const nextFontIndex = (currentFontIndex + 1) % fonts.length;
+    textElement.style.fontFamily = fonts[nextFontIndex];
+  }
+}
+
+// Picture knapp, lager en firkant som kan dras og som kan endre størrelse.
+
+function handlePicturesButtonClick() {
+  const pictureElement = document.createElement("divPicture");
+  pictureElement.style.width = "100px";
+  pictureElement.style.height = "100px";
+  pictureElement.style.backgroundColor = "white";
+  pictureElement.style.border = "2px solid black";
+  pictureElement.style.position = "absolute";
+  pictureElement.style.top = "40%";
+  pictureElement.style.left = "40%";
+  pictureElement.style.transform = "translate(-50%, -50%)";
+  pictureElement.draggable = true;
+  pictureElement.addEventListener("dragstart", dragStart);
+  pictureElement.addEventListener("dragend", dragEnd);
+  pictureElement.addEventListener("mousedown", mouseDown);
+  document.body.appendChild(pictureElement);
+
+  function dragStart(event) {
+    event.dataTransfer.setData("text/plain", event.target.id);
+    event.currentTarget.style.backgroundColor = "gray";
+  }
+
+  function dragEnd(event) {
+    event.currentTarget.style.backgroundColor = "white";
+  }
+
+  function mouseDown(event) {
+    event.preventDefault();
+    const startX = event.clientX;
+    const startY = event.clientY;
+    const startWidth = parseInt(
+      document.defaultView.getComputedStyle(pictureElement).width,
+      10
+    );
+    const startHeight = parseInt(
+      document.defaultView.getComputedStyle(pictureElement).height,
+      10
+    );
+    const rect = pictureElement.getBoundingClientRect();
+    const elemTop = rect.top;
+    const elemLeft = rect.left;
+    const elemRight = rect.right;
+    const elemBottom = rect.bottom;
+    const border = 10;
+    let isDragging = false;
+    let isResizing = false;
+    if (event.clientX - elemLeft < border && event.clientY - elemTop < border) {
+      isResizing = true;
+    } else if (
+      elemRight - event.clientX < border &&
+      elemBottom - event.clientY < border
+    ) {
+      isResizing = true;
+    } else if (event.clientX - elemLeft < border) {
+      isDragging = true;
+      pictureElement.style.cursor = "w-resize";
+    } else if (elemRight - event.clientX < border) {
+      isDragging = true;
+      pictureElement.style.cursor = "e-resize";
+    } else if (event.clientY - elemTop < border) {
+      isDragging = true;
+      pictureElement.style.cursor = "n-resize";
+    } else if (elemBottom - event.clientY < border) {
+      isDragging = true;
+      pictureElement.style.cursor = "s-resize";
+    } else {
+      isDragging = true;
+      pictureElement.style.cursor = "move";
+    }
+    if (isDragging) {
+      document.documentElement.addEventListener("mousemove", dragMove);
+      document.documentElement.addEventListener("mouseup", dragEnd);
+    }
+    if (isResizing) {
+      document.documentElement.addEventListener("mousemove", resizeMove);
+      document.documentElement.addEventListener("mouseup", resizeEnd);
+    }
+
+    function dragMove(event) {
+      const newX = elemLeft + event.clientX - startX;
+      const newY = elemTop + event.clientY - startY;
+      pictureElement.style.left = `${newX}px`;
+      pictureElement.style.top = `${newY}px`;
+    }
+
+    function resizeMove(event) {
+      const newWidth = startWidth + event.clientX - startX;
+      const newHeight = startHeight + event.clientY - startY;
+      pictureElement.style.width = `${newWidth}px`;
+      pictureElement.style.height = `${newHeight}px`;
+      if (
+        event.clientX - elemLeft < border &&
+        event.clientY - elemTop < border
+      ) {
+        pictureElement.style.cursor = "nw-resize";
+      } else if (
+        elemRight - event.clientX < border &&
+        elemBottom - event.clientY < border
+      ) {
+        pictureElement.style.cursor = "se-resize";
+      } else if (event.clientX - elemLeft < border) {
+        pictureElement.style.cursor = "w-resize";
+      } else if (elemRight - event.clientX < border) {
+        pictureElement.style.cursor = "e-resize";
+      } else if (event.clientY - elemTop < border) {
+        pictureElement.style.cursor = "n-resize";
+      } else if (elemBottom - event.clientY < border) {
+        pictureElement.style.cursor = "s-resize";
+      } else {
+        pictureElement.style.cursor = "nwse-resize";
+      }
+    }
+
+    function dragEnd(event) {
+      document.documentElement.removeEventListener("mousemove", dragMove);
+      document.documentElement.removeEventListener("mouseup", dragEnd);
+      pictureElement.style.cursor = "default";
+    }
+
+    function resizeEnd(event) {
+      document.documentElement.removeEventListener("mousemove", resizeMove);
+      document.documentElement.removeEventListener("mouseup", resizeEnd);
+      pictureElement.style.cursor = "default";
+    }
+  }
+}
+
+// Navbar knapp, lager en navbar på skjermen.
+
+function handleNavbarButtonClick() {
+  const navbarElement = document.createElement("divNavbar");
+  navbarElement.style.width = "200px";
+  navbarElement.style.height = "50px";
+  navbarElement.style.backgroundColor = "white";
+  navbarElement.style.border = "2px solid black";
+  navbarElement.style.position = "absolute";
+  navbarElement.style.top = "50%";
+  navbarElement.style.left = "50%";
+  navbarElement.style.transform = "translate(-50%, -50%)";
+  document.body.appendChild(navbarElement);
+}
 
 export default WireframeTool;
