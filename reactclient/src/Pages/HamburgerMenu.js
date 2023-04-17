fbd66f78df3c26c0324d67d211f6354616437ce1
Andreasj3 <andreasjsendal@gmail.com>
2023-04-17 19:35:40 +0200

    uh

--- reactclient/src/Pages/HamburgerMenu.js
+++ reactclient/src/Pages/HamburgerMenu.js
@@ -30,7 +30,7 @@ function HamburgerMenu() {
           ><Link to="/LoggInn">Logg inn</Link></motion.div>
           <motion.div
             whileHover={{scale: 1.1}} whileTap={{scale: 0.9}} onClick={() => null}
-          ><Link to="/Test">Test</Link></motion.div>
+          ><Link to="/WFTest">Test</Link></motion.div>
         </div>
        
         
