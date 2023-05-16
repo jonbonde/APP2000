88784ee2e08bd9dde1d5614e4c921c39b9ba92e4
TTerjesen <tterjesen99+github@gmail.com>
2023-05-16 19:50:13 +0200

    fikset bug

--- reactclient/src/Pages/LagInnlegg.js
+++ reactclient/src/Pages/LagInnlegg.js
@@ -4,7 +4,7 @@ import { motion } from "framer-motion";
 
 export default function LagInnlegg(props) {
     const initialFormData = Object.freeze({
-        placeholder: "",
+        title: "",
         content: "",
         bilde: ""
     });
